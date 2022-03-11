5e00e2a330cf3c317f516c673b5b3552e262a522
RenanNeitzel <renan.vneitzel@gmail.com>
2022-03-11 13:59:40 -0300

    feat(architecture): config babel/ts and adjust paths structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './assets',
+          '@components': './src/components',
+          '@config': './src/config',
+          '@hooks': './src/hooks',
+          '@domain': './src/domain',
+          '@navigation': './src/navigation',
+          '@services': './src/services',
+          '@themes': './src/themes',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
 };
--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 import React from 'react';
 import { NativeBaseProvider } from 'native-base';
 import {AppRegistry} from 'react-native';
-import App from './App';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 export const RootComponent = () => (
