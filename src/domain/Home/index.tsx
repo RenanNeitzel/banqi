d8c1b8be66d85fcdd1c0699587074aa52eef9042
RenanNeitzel <renan.vneitzel@gmail.com>
2022-03-14 13:24:04 -0300

    fix(home): remove unused code

--- src/domain/Home/index.tsx
+++ src/domain/Home/index.tsx
@@ -3,30 +3,15 @@ import {Button, Center, HStack, Image} from 'native-base';
 import {useNavigation} from '@react-navigation/native';
 import {AppNavigatorParamList} from '@navigation/AppNavigator';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
-import {useForm} from 'react-hook-form';
 
 import BanqiLogo from '@assets/images/banqi-logo.png';
 import {Single} from '@components/templates/Single';
-import {CNPJInput, Form} from '@components/common/Form';
 import {StyleSheet} from 'react-native';
 
 type HomeNavigationProps = NativeStackNavigationProp<AppNavigatorParamList>;
 
-type SearchFormValues = {
-  cnpj: string;
-};
-
 export const Home = () => {
   const navigation = useNavigation<HomeNavigationProps>();
-  const {
-    control,
-    handleSubmit,
-    formState: {isValid, isSubmitting},
-  } = useForm<SearchFormValues>({mode: 'onChange'});
-
-  const onSubmit = handleSubmit((formValues: SearchFormValues) => {
-    return navigation.navigate('CompanyDetails', {cnpj: formValues.cnpj});
-  });
 
   return (
     <Single>
